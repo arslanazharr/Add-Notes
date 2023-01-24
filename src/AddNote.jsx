0b0d80058f811e2461c548f85190adafbd7b6125
Arslan <arslanazhar00@gmail.com>
2023-01-24 07:55:19 +0500

    added hook to add note

--- src/AddNote.jsx
+++ src/AddNote.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addnote.css";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 
 const AddNote = () => {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const inputEvent = (event) => {
+    const { name, value } = event.target;
+    setNote((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
   return (
     <>
       <div className="main-note">
         <form>
-          <input type="text" placeholder="Add Title" />
-          <textarea rows="" column="" placeholder="Type here..." />
+          <input
+            type="text"
+            name="title"
+            value={note.title}
+            onChange={inputEvent}
+            placeholder="Add Title"
+          />
+          <textarea
+            rows=""
+            column=""
+            name="content"
+            value={note.content}
+            onChange={inputEvent}
+            placeholder="Type here..."
+          />
           <Button className="button">
             <AddIcon />
           </Button>
